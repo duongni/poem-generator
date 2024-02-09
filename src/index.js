ae04e8faf8d80f644eb3c69fff2ad18d17666616
duongni <133175915+duongni@users.noreply.github.com>
2024-02-08 22:02:01 -0500

    update the language will be display in javascript

--- src/index.js
+++ src/index.js
@@ -1,24 +1,43 @@
+function displayPoem(response) {
+  if (response.data && response.data.answer) {
+    var poem = document.getElementById("poem");
+
+    var typewriter = new Typewriter(poem, {
+      loop: false,
+      delay: 75,
+    });
+
+    typewriter
+      .pauseFor(100)
+      .typeString("Generating poem ...")
+      .pauseFor(300)
+      .deleteChars(19)
+      .typeString(`${response.data.answer}`)
+
+      .pauseFor(1000)
+      .start();
+  } else {
+    console.log("Failed to generate poem: Invalid response format");
+  }
+}
+
 function generatePoem(event) {
   event.preventDefault();
 
-  var poem = document.getElementById("poem");
+  let prompt = document.querySelector("#prompt");
+  let context =
+    "You are a Vietnamese romantic poet expert, create a 4-6 lines short and beautiful poem in Vietnamese, each line should be on a separate line.";
+  let apiKey = "a0a06a4d0t0a9fff1oce244a979b7153";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  var typewriter = new Typewriter(poem, {
-    loop: false,
-    delay: 75,
-  });
-  console.log(poem);
-  typewriter
-    .pauseFor(2500)
-    .typeString("Generating poem ...")
-    .pauseFor(300)
-    .deleteChars(19)
-    .typeString(
-      "Sometimes, when the light strikes at odd angle <br/> and pulls you back into childhood <br/>and you are passing a crumbling mansion <br/>completely hidden behind old willows"
-    )
+  axios
+    .get(apiUrl)
+    .then(displayPoem)
+    //check response has any data
 
-    .pauseFor(1000)
-    .start();
+    .catch((error) => {
+      console.error("Failed to generate poem:", error.message);
+    });
 }
 
 let poemFormElement = document.querySelector("#poem-form");
